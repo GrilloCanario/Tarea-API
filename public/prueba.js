abd0e3054c75afce8227f001b041c12530b50f17
Pedro Barrera <pedrodbarrerafdez@gmail.com>
2025-12-03 18:42:30 +0000

    update clase + grid ejemplo

---
+++ public/prueba.js
@@ -0,0 +1,6 @@
+const episodios = document.querySelector(".episodios");
+const episodio = document.querySelector(".episodio");
+
+episodios.addEventListener("click", () => {
+    episodio.classList.remove("oculto");
+});

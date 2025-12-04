afdec5fc2f2dac10073469eb03473391d1ecef3b
PedroBarrera <pedrodbarrerafdez@gmail.com>
2025-12-04 14:17:23 +0000

    casa

--- public/prueba.js
+++ public/prueba.js
@@ -1,6 +1,11 @@
 const episodios = document.querySelector(".episodios");
 const episodio = document.querySelector(".episodio");
+const personaje = document.querySelector(".personaje");
 
 episodios.addEventListener("click", () => {
     episodio.classList.remove("oculto");
+});
+
+episodio.addEventListener("click", () => {
+    personaje.classList.remove("oculto");
 });

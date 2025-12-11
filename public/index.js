a0d6555a2bbdd7aae0fee0b1319bfd335258ad6a
Pedro Barrera <pedrodbarrerafdez@gmail.com>
2025-12-11 16:46:28 +0000

    character-info updated

--- public/index.js
+++ public/index.js
@@ -14,8 +14,11 @@ episodio.addEventListener("change", () => {
 //* Listado de Capitulos
 const episodesList = document.getElementById('episode-list');
 const episodeInfo = document.getElementById('episode-info');
-const epsTotal = [];
+const characterList = document.getElementById('character-list');
+const characterInfo = document.getElementById('character-info');
+const clearStorage = document.getElementById('clear-storage');
 
+const epsTotal = [];
 loadEpisodes();
 async function loadEpisodes() {
     try {
@@ -42,7 +45,7 @@ function insertEpisodes(episode) {
 }
 
 //* Info del capitulo escogido
-episodesList.addEventListener("change", () => {
+episodesList.addEventListener("change", async () => {
     const id = episodesList.value;
     const episodio = epsTotal.find(ep => ep.id == id);
 
@@ -54,6 +57,47 @@ episodesList.addEventListener("change", () => {
         <p><strong>Episodio:</strong> ${episodio.episode}</p>
         <p><strong>Fecha de emisión:</strong> ${episodio.air_date}</p>
         <p><strong>Personajes en el episodio:</strong> ${episodio.characters.length}</p>
+    
     `;
-    episodeInfo.append(div);
+    const idcharslist = episodio.characters.map(pito => pito.slice(42));
+    let response = await fetch('https://rickandmortyapi.com/api/character/' + idcharslist.join(","));
+    if (!response.ok) {
+        throw new Error('Error en la red');
+    }
+    let characters = await response.json();
+    
+    characterList.textContent="";
+    characters.forEach(character => {
+        insertCharacters(character);
+    });
+    
+});
+
+function insertCharacters(character) {
+    const option = document.createElement('option');
+    option.value = character.id;
+    option.textContent = character.name;
+    characterList.append(option);
+   
+}
+characterList.addEventListener("change", async () => {
+    const id = characterList.value; 
+
+    // petición a la API
+    const response = await fetch('https://rickandmortyapi.com/api/character/' + id);
+    const character = await response.json();
+
+    // mostramos en el div de personaje
+    characterInfo.innerHTML = `
+        <p><strong>Nombre:</strong> ${character.name}</p>
+        <p><strong>Estado:</strong> ${character.status}</p>
+        <p><strong>Especie:</strong> ${character.species}</p>
+        <p><img src="${character.image}" width="200"/></p>
+    `;
+    characterList.addEventListener("change", async () => {
+    const id = characterList.value;
+
+   
+});
+
 });
